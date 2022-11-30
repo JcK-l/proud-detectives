e52d187eaf3715262723ecb8dd8023d41d31f335
9wolpers <daniel.wolpers@studium.uni-hamburg.de>
2022-11-30 21:39:54 +0100

    backend-12: including pseudonym of sender for ChatMessage broadcast

--- src/main/java/de/uhh/detectives/backend/service/impl/ChatMessageServiceImpl.java
+++ src/main/java/de/uhh/detectives/backend/service/impl/ChatMessageServiceImpl.java
@@ -2,7 +2,9 @@ package de.uhh.detectives.backend.service.impl;
 
 import de.uhh.detectives.backend.model.entity.ChatMessage;
 import de.uhh.detectives.backend.model.Message;
+import de.uhh.detectives.backend.model.entity.Player;
 import de.uhh.detectives.backend.repository.ChatMessageRepository;
+import de.uhh.detectives.backend.repository.PlayerRepository;
 import de.uhh.detectives.backend.service.api.ChatMessageService;
 import de.uhh.detectives.backend.service.api.MessageType;
 import de.uhh.detectives.backend.service.impl.adapter.ChatMessageAdapter;
@@ -10,16 +12,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ChatMessageServiceImpl implements ChatMessageService {
 
     private static final Logger LOG = LoggerFactory.getLogger(ChatMessageServiceImpl.class);
 
     private final ChatMessageRepository chatMessageRepository;
+    private final PlayerRepository playerRepository;
+
     private final ChatMessageAdapter chatMessageAdapter;
 
-    public ChatMessageServiceImpl(final ChatMessageRepository chatMessageRepository) {
+    public ChatMessageServiceImpl(final ChatMessageRepository chatMessageRepository, final PlayerRepository playerRepository) {
         this.chatMessageRepository = chatMessageRepository;
+        this.playerRepository = playerRepository;
         this.chatMessageAdapter = new ChatMessageAdapter();
     }
 
@@ -32,6 +39,8 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     public String handle(final Message message) {
         final ChatMessage chatMessage = (ChatMessage) message;
         saveMessage(chatMessage);
+        final Optional<Player> sender = playerRepository.findById(chatMessage.getSenderId());
+        sender.ifPresent(player -> chatMessage.setSenderPseudonym(player.getPseudonym()));
         return chatMessageAdapter.toBroadcastString(chatMessage);
     }
 
--- src/test/java/de/uhh/detectives/backend/service/impl/ChatMessageServiceImplTest.java
+++ src/test/java/de/uhh/detectives/backend/service/impl/ChatMessageServiceImplTest.java
@@ -1,8 +1,10 @@
 package de.uhh.detectives.backend.service.impl;
 
-import de.uhh.detectives.backend.model.entity.ChatMessage;
 import de.uhh.detectives.backend.model.EmptyMessage;
+import de.uhh.detectives.backend.model.entity.ChatMessage;
+import de.uhh.detectives.backend.model.entity.Player;
 import de.uhh.detectives.backend.repository.ChatMessageRepository;
+import de.uhh.detectives.backend.repository.PlayerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -10,10 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ChatMessageServiceImplTest {
@@ -23,6 +29,9 @@ public class ChatMessageServiceImplTest {
 
     @Mock
     private ChatMessageRepository chatMessageRepository;
+
+    @Mock
+    private PlayerRepository playerRepository;
 
     @Test
     public void testAccepts() {
@@ -45,6 +54,10 @@ public class ChatMessageServiceImplTest {
         chatMessage.setMessageContent("this is a test message");
         chatMessage.setTimestamp(1669505325493L);
 
+        final Player sender = new Player();
+        sender.setPseudonym("Mr. Tester");
+        when(playerRepository.findById(anyLong())).thenReturn(Optional.of(sender));
+
         final ArgumentCaptor<ChatMessage> captor = ArgumentCaptor.forClass(ChatMessage.class);
 
         // when
@@ -54,6 +67,7 @@ public class ChatMessageServiceImplTest {
         verify(chatMessageRepository).save(captor.capture());
         final ChatMessage actual = captor.getValue();
         assertEquals(1669494958668L, actual.getSenderId());
+        assertEquals("Mr. Tester", actual.getSenderPseudonym());
         assertEquals("tester", actual.getReceiverPseudonym());
         assertEquals("this is a test message", actual.getMessageContent());
         assertEquals(1669505325493L, actual.getTimestamp());
