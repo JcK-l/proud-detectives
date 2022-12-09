a892dd780c4bdc969a687ef6831507c3bfff8f01
9wolpers <daniel.wolpers@studium.uni-hamburg.de>
2022-12-10 00:44:11 +0100

    fixed test for location generation

--- src/test/java/de/uhh/detectives/backend/service/impl/LocationGeneratorImplTest.java
+++ src/test/java/de/uhh/detectives/backend/service/impl/LocationGeneratorImplTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.withSettings;
 @ExtendWith(MockitoExtension.class)
 public class LocationGeneratorImplTest {
 
-    private static  final Double firstRandomDouble = 900.4004227046;
+    private static final Double firstRandomDouble = 900.4004227046;
     private static final Double[] randomDoubles = new Double[] {
             726.2895275915, 1105.499235166, 28.2679072853, 1411.0204422618,                 // distances
             222.3241122218, 490.7678870255, 804.6275513687, 848.3999673177, 427.4845146097, // distances
@@ -39,16 +39,16 @@ public class LocationGeneratorImplTest {
         when(random.nextDouble(anyDouble(), anyDouble())).thenReturn(firstRandomDouble, randomDoubles);
 
         final Point[] pointsArray = new Point[] {
-            new Point(9.99474794474539, 53.53997072094688),
-            new Point(9.994578287257859, 53.540114071758865),
-            new Point(9.994598539144068, 53.54017787785358),
-            new Point(9.994614413481091, 53.54000217320384),
-            new Point(9.994616940451891, 53.540226147907084),
-            new Point(9.994644644956649, 53.53999588723187),
-            new Point(9.994539190192864, 53.5400326393949),
-            new Point(9.994692717458273, 53.54010110978595),
-            new Point(9.994716793939258, 53.53992437037605),
-            new Point(9.99465713114555, 53.539956378447265)
+                new Point(10.007838704127186, 53.53803726955933),
+                new Point(9.99145061150512, 53.54626607784871),
+                new Point(9.99340704852899, 53.54992875763334),
+                new Point(9.994940727463357, 53.53984273259902),
+                new Point(9.995184996212444, 53.55269961753237),
+                new Point(9.997860929136815, 53.53948189712467),
+                new Point(9.987674197628989, 53.54159159221192),
+                new Point(10.002505611030712, 53.5455220179535),
+                new Point(10.004829157589795, 53.53537659428126),
+                new Point(9.999066791835823, 53.537213962888366)
         };
         final List<Point> expected = Arrays.asList(pointsArray);
 
